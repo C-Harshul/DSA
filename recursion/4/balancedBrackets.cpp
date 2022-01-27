70bddbfb9ea5cafcb51aee90e57dea66e370481b
C-Harshul <chandrasekharharshul@gmail.com>
2022-01-28 01:04:18 +0530

    27 Jan 2022

---
+++ recursion/4/balancedBrackets.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void balancedBracket(int o, int c, char *s, int n, int i = 0)
+{
+
+    if (i == n)
+    {
+        s[i] = '\0';
+        cout << s << endl;
+        return;
+    }
+    if (o < c)
+    {
+        if (c > 0)
+        {
+
+            s[i] = ')';
+            balancedBracket(o, c - 1, s, n, i + 1);
+        }
+        if (o > 0)
+        {
+
+            s[i] = '(';
+            balancedBracket(o - 1, c, s, n, i + 1);
+        }
+    }
+    else
+    {
+        if (o > 0)
+        {
+
+            s[i] = '(';
+            balancedBracket(o-1, c, s, n, i + 1);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    char s[10000];
+    balancedBracket(n, n, s, 2 * n);
+    return 0;
+}
