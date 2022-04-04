3f585c6669aeef53135598b5df74943a70dac826
C-Harshul <chandrasekharharshul@gmail.com>
2022-04-04 12:09:37 +0530

    4 April 2022

---
+++ lab_task_3/question1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+void heapify(int a[], int n, int i)
+{
+    int largest = i;       
+    int left = 2 * i + 1;  
+    int right = 2 * i + 2; 
+    if (left < n && a[left] > a[largest])
+        largest = left;
+    if (right < n && a[right] > a[largest])
+        largest = right; 
+    if (largest != i)
+    {
+        int temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
+        heapify(a, n, largest);
+    }
+}
+void heapSort(int a[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(a, n, i);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int temp = a[0];
+        a[0] = a[i];
+        a[i] = temp;
+        heapify(a, i, 0);
+    }
+} 
+void printArr(int a[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        cout << a[i] << " ";
+    }
+}
+int main()
+{
+    int a[1000];
+    int n; 
+    
+    // Get the number of elements in array 
+    cout<<"Enter the number elements you want to add to array : ";
+    cin>>n;
+    // Take the input for the array
+    for(int i = 0;i<n;++i){
+       cout<<"Enter element "<<i + 1<<" : ";
+        cin>>a[i];
+    }
+    cout << "Before sorting array elements are - \n";
+    printArr(a, n);
+    heapSort(a, n);
+    cout << "\nAfter sorting array elements are - \n";
+    printArr(a, n);
+    return 0;
+}
